412b706d4b4e0d878427762cb36ccad7e81d8241
yuan1yue <109784555+yuan1yue@users.noreply.github.com>
2023-02-01 12:49:43 +0800

    love

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int main()
+{
+		FILE *fp;
+		const char* p[4]={"p0.txt","p1.txt","p2.txt","p3.txt"};
+		for(int i=0;;i=(i+1)%4)
+		{
+			fp = fopen(p[i],"r+");
+			while(!feof(fp))
+			{
+				printf("%c",fgetc(fp));
+			}
+			fclose(fp);
+			Sleep(10000);
+			system("cls");       
+		}
+	return 0;
+}
